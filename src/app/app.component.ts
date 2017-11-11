19ea662466980b9024225a75e50dcce7907be41a
jstivenbernal <jeisson.bernal@FSA.COM>
2017-11-11 17:33:35 -0500

    git prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hola mundo subido';
+  title = 'Hola mundo subido a git';
 }
