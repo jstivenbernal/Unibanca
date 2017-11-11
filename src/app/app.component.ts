08f486855eec79f864b786d334818621c411d064
jstivenbernal <stiven_941202@hotmail.com>
2017-11-11 17:34:50 -0500

    Update app.component.ts
    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hola mundo subido a git';
+  title = 'Hola mundo subido a git 2';
 }
